d968ec399e565ed0c96db22f5b667101b06ce790
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-08-02 22:55:14 +0530

    Update 35.js

--- cdn/js/35.js
+++ cdn/js/35.js
@@ -1,8 +1,19 @@
-$('document').ready(function() {
-  $.getJSON("https://spreadsheets.google.com/feeds/list/1uT8y69qqzCxdHjh7_nR-41wwLAdnT2SUWYDJ8RyP6Hg/1/public/values?alt=json", function(data) {
-    //start each
-    var entry = data.feed.entry[Math.floor(Math.random() * data.feed.entry.length)]; 
-    $('#kasads').html('<div class="bc-banner text-center" style="background-image: url('+ entry.gsx$bgimg.$t +');background-position: center center;background-size: 100% auto;"><a href="https://adsforexchange.blogspot.com/"><img src="https://s.clipartkey.com/mpngs/s/103-1030941_info-icon-blue-png-clipart-png-download-info.png" alt="Ads For Exchange" class="bc-info"/> </a><a href="' + entry.gsx$url.$t +'"> <div class="bc-banner-body"> <h2 style="color:'+ entry.gsx$txtcolor.$t +';">'+ entry.gsx$htxt.$t +'</h'+'2>  <p style="color:'+ entry.gsx$txtcolor.$t +';">'+ entry.gsx$ptxt.$t +'</p>  <center><span class="bc-btn" style="background: '+ entry.gsx$btnbgcolor.$t +';"><h3 style="color:'+ entry.gsx$btntxtcolor.$t +'">'+ entry.gsx$btntxt.$t +'</h3></span></cen'+'ter> </di'+'v> </d'+'iv></a>'); 
-  });
-});
-document.write('<link rel="stylesheet" type="text/css" href="https://kasworld-aero.github.io/old/virus/rads/new-min.css">');
+function loadjscssfile(filename, filetype){
+    if (filetype=="js"){ //if filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype=="css"){ //if filename is an external CSS file
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+ 
+loadjscssfile("https://kasworld-aero.github.io/old/virus/rads/fontawesome.js", "js") //dynamically load and add this .js file
+//loadjscssfile("javascript.php", "js") //dynamically load "javascript.php" as a JavaScript file
+loadjscssfile("https://kasworld-aero.github.io/old/virus/rads/new-min.css", "css") ////dynamically load and add this .css file
